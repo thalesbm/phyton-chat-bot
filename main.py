1642738dbbd336c8273a41ba2e97ab28970b2a77
Thales Marega <thales.bm92@gmail.com>
2025-04-13 12:02:48 -0300

    first commit

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from excel_utils import readExcel
 
 # load api key
 load_dotenv()
+
+# criar um arquivo .env e adicionar a chave do open ai
 api_key = os.getenv("OPENAI_API_KEY")
 
 # readPDF(api_key)
